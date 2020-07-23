f5e6206c8e110ef8872e0b7051677b7d8ca3b08f
Ryota-1 <stream.of.tears@icloud.com>
2020-07-23 15:32:20 +0900

    [Finish JS-2]

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-aleat('Hello World');
+// 条件分岐
+var orange = 100;
+var apple = 120;
+if(orange < apple){
+	alert('みかんの値段がリンゴより安い');
+}
+else if(orange == apple){
+	alert('みかんとりんごが同じ値段nedann');
+}
+else{
+	alert('みかんの値段がリンゴより高い');
+}
