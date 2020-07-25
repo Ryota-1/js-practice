fa17a6d2619e95a89384cd544e2e43e1f54f7a20
Ryota-1 <stream.of.tears@icloud.com>
2020-07-25 16:39:22 +0900

    [Finish JS-3]

---
+++ JS-3/app.js
@@ -0,0 +1,13 @@
+// 簡単な関数の記述
+// メイン部分
+var alertString;
+alertString = addString("WebCamp");
+
+// 作成した関数を呼び出す
+alert(alertString);
+
+// 作成した関数
+function addString(strA){
+	var addStr = "Hello " + strA;
+	return addStr;
+}
