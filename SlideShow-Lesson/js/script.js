8b365d6b47ddde73ed72b7a2fc361138aa8d7d54
Ryota-1 <stream.of.tears@icloud.com>
2020-07-30 22:36:40 +0900

    true

--- SlideShow-Lesson/js/script.js
+++ SlideShow-Lesson/js/script.js
@@ -19,5 +19,5 @@ $(".theTarget").skippr({
 	// キーボードの矢印キーによるスライド送りの設定(trueで有効)
 	keyboardOnAlways : true,
 	// 1枚目のスライド表示時に戻る矢印を表示するかどうか [false]:矢印を隠さない [true]:矢印を隠す
-	hidePrevious : false
+	hidePrevious : true
 });
