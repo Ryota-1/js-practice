8dc4438573371d33eb2f3b3cec5f8cc58474cc6c
Ryota-1 <66394296+Ryota-1@users.noreply.github.com>
2020-10-18 21:32:52 +0900

    Update script.js

--- Ajax-Lesson/script.js
+++ Ajax-Lesson/script.js
@@ -5,9 +5,7 @@ $(function(){
 	$('#btn').on('click', function() {
 		//入力された都市名でWebAPIに天気情報をリクエスト
 		$.ajax({
-			url: "http://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
-			dataType : 'jsonp',
-		}).done(function (data){
+			url:
 			// 通信成功
 			// 位置
 			$('#place').text(data.name);
